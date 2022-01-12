d43d485d84de75ba24ff1fc8ff79d33d0154c0c3
wsjoshua <ws.joshua@gmail.com>
2022-01-12 07:01:22 -0600

    updated wk2 prompts

---
+++ wk2/problems/3_separate_names.js
@@ -0,0 +1,16 @@
+/*
+Write a function that takes in a name (first and last separated with a space) and returns an array with the two names separated as well as the first letter of each name capitalized 
+Examples: 
+
+example input: "ryan lugtu"
+example output: ["Ryan", "Lugtu"]
+*******************************************************************************
+*/
+
+
+function separateNames(name) {
+    // your code here...
+  }
+  
+  /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+  module.exports = separateNames;
--- wk2/test/test.js
+++ wk2/test/test.js
@@ -5,10 +5,8 @@ var assert = require('assert');
 
 var reverseOddRange = tryRequire('../problems/1_reverse_odd_range.js');
 var isSquare = tryRequire('../problems/2_is_square.js');
-var mysticNumbers = tryRequire('../problems/3_mystic_numbers.js');
-var firstOrLast = tryRequire('../problems/4_first_or_last.js');
-// var fromMeToYou = tryRequire('../problems/5_from_me_to_you.js');
-var intersect = tryRequire('../problems/5_intersect.js');
+var separateNames = tryRequire('../problems/3_separate_names.js');
+
 
 describe('reverseOddRange()', function () {
   it('should return an array of odd numbers between "start" and "end" in reverse order', function () {
@@ -21,70 +19,21 @@ describe('reverseOddRange()', function () {
   });
 });
 
-
-// describe('isSquare()', function () {
-//   it('should return a boolean indicating whether or not a number is a perfect square', function () {
-//     assert.equal(isSquare(7), false);
-//     assert.equal(isSquare(15), false);
-//     assert.equal(isSquare(18), false);
-//     assert.equal(isSquare(25), true);
-//     assert.equal(isSquare(100), true);
-//   });
-// });
-
-
-describe('mysticNumbers()', function () {
-  it('should return an array of numbers less than max that are divisible by 4 or 6, but not both', function () {
-    var result1 = mysticNumbers(25);
-    var expected1 = [ 20, 18, 16, 8, 6, 4 ];
-    assert.deepEqual(result1.sort(), expected1.sort());
-
-    var result2 = mysticNumbers(14);
-    var expected2 = [ 8, 6, 4 ];
-    assert.deepEqual(result2.sort(), expected2.sort());
+describe('isSquare()', function () {
+  it('should return a boolean indicating whether or not a number is a perfect square', function () {
+    assert.equal(isSquare(7), false);
+    assert.equal(isSquare(15), false);
+    assert.equal(isSquare(18), false);
+    assert.equal(isSquare(25), true);
+    assert.equal(isSquare(100), true);
   });
+});
 
-  it('should return the array of numbers in reverse order', function () {
-    var result1 = mysticNumbers(25);
-    var expected1 = [ 20, 18, 16, 8, 6, 4 ];
-    assert.deepEqual(result1, expected1);
-
-    var result2 = mysticNumbers(14);
-    var expected2 = [ 8, 6, 4 ];
-    assert.deepEqual(result2, expected2);
+describe('separateNames()', function () {
+  it('should return an array containing two strings (the first and last name) with the first characters capitalizied', function () {
+    assert.deepEqual(separateNames("john doe"), [ "John", "Doe" ]);
+    assert.deepEqual(separateNames("ryan lugtu"), [ "Ryan","Lugtu" ]);
   });
 });
 
 
-describe('firstOrLast()', function () {
-  it('should return the first element if there are an even number of elements in the array', function () {
-    assert.equal(firstOrLast(['a', 'b', 'c', 'd']), 'a');
-    assert.equal(firstOrLast([1, 2]), 1);
-  });
-
-  it('should return the last element if there are an odd number of elements in the array', function () {
-    assert.equal(firstOrLast(['Jenny', 'Mary', 'Mark']), 'Mark');
-    assert.equal(firstOrLast([1, 2, 3]), 3);
-  });
-});
-
-
-// describe('fromMeToYou()', function () {
-//   it('should return the sentence where "me" is replaced with "you"', function () {
-//     assert.equal(fromMeToYou('that made me laugh'), 'that made you laugh');
-//     assert.equal(fromMeToYou('love me or hate me'), 'love you or hate you');
-//   });
-// });
-
-
-describe('intersect()', function () {
-  it('should return an array containing the common elements between arr1 and arr2', function () {
-    var result1 = intersect(['a', 'b', 'c', 'd'], ['b', 'd', 'e']);
-    var answer1 = [ 'b', 'd' ];
-    assert.deepEqual(result1.sort(), answer1);
-
-    var result2 = intersect(['a', 'b', 'c'], ['x', 'y', 'z']);
-    var answer2 = []
-    assert.deepEqual(result2, answer2);
-  });
-});
