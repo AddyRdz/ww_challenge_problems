b3942009613fb1b6534c5ea43df7c431b4e0b04e
wsjoshua <ws.joshua@gmail.com>
2022-02-02 07:40:51 -0600

    added problems without tests

---
+++ wk7/problems/1_findWordFrequencies.js
@@ -0,0 +1,19 @@
+/******************************************************************************
+Write a function findWordFrequencies that takes in a sentence (string), and returns an object with each word as a key, with a value of how many times that word appears in the sentence.
+
+eg: { I: 1, love: 1, lamp: 1 }
+
+Make it work just for sentences without commas, apostrophes, and periods. Capital letters and lowercase letters should be treated as equals: 'The' and 'the' are the same word.
+
+Examples:
+
+findWordFrequencies('The world is all that is the case') => { the: 2, world: 1, is: 2, all: 1, that: 1, case: 1 }
+
+findWordFrequencies('That that is is that that is not is not') => { that: 4, is: 4, not: 2 }
+*******************************************************************************/
+
+function findWordFrequencies(array) {
+  // your code here...
+}
+
+
---
+++ wk7/problems/2_findHighestFrequency.js
@@ -0,0 +1,17 @@
+/*******************************************************************************
+Write a function findHighestFrequency that takes in an object and returns an object with the key that has the highest value. With this function we can see which word appeared in the sentence with the highest frequency.
+
+If there is a tie between the two most frequent words, the first appearing one is returned.
+
+Examples:
+
+findHighestFrequency( { that: 4, is: 4, not: 2 } ) => { that: 4 }
+findHighestFrequency( { hi: 1 } ) => { hi: 1 }
+*******************************************************************************/
+
+function findHighestFrequency(freqObject) {
+  // your code here...
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = mysticNumbers;
